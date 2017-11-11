48c6a8a7c513c2a9dcf70c8fe2a7358d484b6ff4
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-12 01:42:37 +0300

    Add test on negative widget width size

--- tests/src/initial_data_test.cpp
+++ tests/src/initial_data_test.cpp
@@ -1,5 +1,28 @@
-﻿#include <initial_data.h>
+﻿#include <stdexcept>
+#include <string>
 
 #include <doctest.h>
 
-SCENARIO("correct storing and obtaining initial datas") {}
+#include <initial_data.h>
+
+SCENARIO("correct storing and obtaining initial datas") {
+  GIVEN("integer number that contains negative widget width size") {
+    int negative_widget_width = -100;
+
+    WHEN(
+        "method SetInitialWidgetWidth of InitialData class receive\n"
+        "\t    negative widget width size as parameter") {
+      InitialData initial_data;
+      std::string error_text;
+      try {
+        initial_data.SetInitialWidgetWidth(negative_widget_width);
+      } catch (const std::logic_error& error) {
+        error_text = error.what();
+      }
+      THEN("text of exception should be thrown") {
+        REQUIRE(error_text ==
+                std::string("widget sides size must be greater than 0"));
+      }
+    }
+  }
+}
