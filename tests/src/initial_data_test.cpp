8e9cafd3662b76318784498d451e95fab705dfd1
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-12 21:47:53 +0300

    Change test description

--- tests/src/initial_data_test.cpp
+++ tests/src/initial_data_test.cpp
@@ -6,20 +6,19 @@
 #include <initial_data.h>
 
 SCENARIO("correct storing and obtaining initial datas") {
-  GIVEN("integer number that contains negative widget width size") {
-    int negative_widget_width = -100;
+  GIVEN("instance of InitialData class and string for error text") {
+    InitialData initial_data;
+    std::string error_text;
 
     WHEN(
-        "method SetInitialWidgetWidth of InitialData class receive\n"
-        "\t    negative widget width size as parameter") {
-      InitialData initial_data;
-      std::string error_text;
+        "method SetInitialWidgetWidth receive negative widget width size as "
+        "parameter") {
       try {
-        initial_data.SetInitialWidgetWidth(negative_widget_width);
+        initial_data.SetInitialWidgetWidth(-100);
       } catch (const std::logic_error& error) {
         error_text = error.what();
       }
-      THEN("text of exception should be thrown") {
+      THEN("text of exception should be written") {
         REQUIRE(error_text ==
                 std::string("widget sides size must be greater than 0"));
       }
