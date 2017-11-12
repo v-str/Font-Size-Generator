52d349f9b4053f500c82645bfeaee594cabf5756
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-12 22:24:24 +0300

    Remade body of the test

--- tests/src/initial_data_test.cpp
+++ tests/src/initial_data_test.cpp
@@ -5,22 +5,16 @@
 
 #include <initial_data.h>
 
-SCENARIO("correct storing and obtaining initial datas") {
-  GIVEN("instance of InitialData class and string for error text") {
+SCENARIO("correct storing and obtaining initial data") {
+  GIVEN("instance of InitialData class") {
     InitialData initial_data;
-    std::string error_text;
 
     WHEN(
         "method SetInitialWidgetWidth receive negative widget width size as "
         "parameter") {
-      try {
-        initial_data.SetInitialWidgetWidth(-100);
-      } catch (const std::logic_error& error) {
-        error_text = error.what();
-      }
-      THEN("text of exception should be written") {
-        REQUIRE(error_text ==
-                std::string("widget sides size must be greater than 0"));
+      THEN("exception should be thrown") {
+        REQUIRE_THROWS_AS(initial_data.SetInitialWidgetWidth(-100),
+                          const std::logic_error&);
       }
     }
   }
