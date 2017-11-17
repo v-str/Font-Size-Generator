9297b69d24cb38c61d69b46e07bb912464b7bfa5
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-17 23:38:35 +0300

    Travis CI : test on failure #2 Remove QApplication

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 ﻿#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.h>
 
-#include <QApplication>
-
 int main(int argc, char** argv) {
   doctest::Context context;
   context.applyCommandLine(argc, argv);
   int test_launcher = context.run();
 
-  QApplication app(argc, argv);
-
   if (context.shouldExit()) {
-    return test_launcher = app.exec();
+    return test_launcher;
   }
 }
