53282e19fb6080bc1bc116e8e9fc9218b55b5050
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-17 23:50:29 +0300

    Travis CI : test on failure #3 Change main launching

--- main.cpp
+++ main.cpp
@@ -1,12 +1,2 @@
-﻿#define DOCTEST_CONFIG_IMPLEMENT
-#include <doctest.h>
-
-int main(int argc, char** argv) {
-  doctest::Context context;
-  context.applyCommandLine(argc, argv);
-  int test_launcher = context.run();
-
-  if (context.shouldExit()) {
-    return test_launcher;
-  }
-}
+﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
