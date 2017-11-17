75583b35b923fe543c1a01019fcee8b84f80fda9
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-17 11:53:58 +0300

    Remove untested code

--- library/font_controller.cpp
+++ library/font_controller.cpp
@@ -3,12 +3,6 @@
 #include <QWidget>
 
 void FontController::SetInitialWidgetMeasurements(double font_scale_multiplier,
-                                                  const QWidget& widget) {
-  initial_data_.SetInitialWidgetWidth(widget.width());
-  initial_data_.SetInitialWidgetHeight(widget.height());
-  initial_data_.SetInitialFontPixelSize(widget.font().pixelSize());
-  initial_data_.SetFontScaleMultiplier(font_scale_multiplier);
-  widget_font_ = widget.font();
-}
+                                                  const QWidget& widget) {}
 
 void FontController::ControllFontSize(QWidget* widget) {}
