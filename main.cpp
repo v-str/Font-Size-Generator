c34b366378cb585a4908ac13c73f9b1aa9b7a2af
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-19 10:28:13 +0300

    Set custom doctest main function Using qt

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
-﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+﻿#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
+
+#include <QApplication>
+
+int main(int argc, char** argv) {
+  doctest::Context context;
+  QApplication app(argc, argv);
+  context.applyCommandLine(argc, argv);
+  int result_of_tests = context.run();
+  if (context.shouldExit()) {
+    return result_of_tests;
+  }
+}
