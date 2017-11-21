74df0cc327e377b201774ce70ad596a7b62a56a6
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-21 16:19:35 +0300

    Change initial values to negative Perform initial setting for font controller

--- library/font_controller.cpp
+++ library/font_controller.cpp
@@ -5,6 +5,11 @@
 #include <QWidget>
 
 void FontController::SetInitialParameters(double font_scale_multiplier,
-                                          const QWidget& widget) {}
+                                          const QWidget& widget) {
+  initial_data_.SetInitialWidgetWidth(widget.width());
+  initial_data_.SetInitialWidgetHeight(widget.height());
+  initial_data_.SetInitialFontPixelSize(widget.font().pixelSize());
+  initial_data_.SetFontScaleMultiplier(font_scale_multiplier);
+}
 
 void FontController::ControllFontSize(QWidget* widget) {}
--- library/initial_data.h
+++ library/initial_data.h
@@ -18,10 +18,10 @@ class InitialData {
   int ReturnZeroIfInputValutNegative(int input_value) const;
   double ReturnZeroIfMultiplierNegative(double font_scale_multiplier) const;
 
-  int initial_widget_width_ = 0;
-  int initial_widget_height_ = 0;
-  int initial_font_pixel_size_ = 0;
-  double font_scale_multiplier_ = 1.0;
+  int initial_widget_width_ = -1;
+  int initial_widget_height_ = -1;
+  int initial_font_pixel_size_ = -1;
+  double font_scale_multiplier_ = -1.0;
 };
 
 #endif  // INITIAL_DATA_H
